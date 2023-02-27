d68d577c1693012508c8945ca048ec999cbd1791
JHLvJz <jhsu1619@gmail.com>
2023-02-27 21:44:08 +0900

    버전 변경 & 세팅 축소

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,31 @@
-const path = require('path');
+const path = require("path");
 
-const HtmlwebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); //추가
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //추가
 
 module.exports = {
-    mode: 'development',
-
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-    
-    entry: './src/index',
-
-    output: {
-        path: path.resolve(__dirname, 'build'), 
-        __filename: 'bundle.js',
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\\.(ts|js)x?$/, 
-                user: [
-                    {
-                        loader: 'babel-loader',
-                    },
-                ],
-                exclude: /node_modules/, 
-            },
-            {
-                test: /\\.tsx$/,
-                use: ['ts-loader'],
-                exclude: ['/node_modules/'],
-            },
-            {
-                test: /\\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\\.(png|jpe?g|gif|woff|woff2|ttf|svg|ico)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-        ],
-    },
-
-    devServer: {
-        static: path.join(__dirname, 'build'),
-        port: 7777,
-    },
-
-    plugins: [
-        new HtmlwebpackPlugin({
-            template: `./public/index.html`,
-        }),
-
-        new CleanWebpackPlugin(),
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.min.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // 웹팩 실행시마다 dist 폴더 정리
+    new HtmlWebpackPlugin({
+      //index.html 자동 생성되도록 template 옵션 설정
+      template: "./src/index.html",
+    }),
+  ],
 };
